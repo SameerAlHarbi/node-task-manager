70b1b752faebde3933b68b0ba9192e423f4516d8
Sameer Al Harbi <smrhrbi@gmail.com>
2020-02-10 01:20:42 +0300

    init

---
+++ mongodb.js
@@ -0,0 +1,23 @@
+//CRUD creadte read update delete
+
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, {useUnifiedTopology: true}, (error, client) => {
+    
+    if(error) {
+        return console.log('Unable to connect to database!');
+    }
+
+    console.log('Connected correctly');
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'Sameer',
+        age: 27
+    });
+});
