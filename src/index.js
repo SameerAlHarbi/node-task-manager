dcc8342b989dc8c671ec3b856f43685ddc808362
Sameer Al Harbi <smrhrbi@gmail.com>
2020-02-13 02:25:00 +0300

    model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const chalk = require('chalk');
+require('./db/mongoose'); //just ensure the file will loaded and connect to db
+const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,8 +10,23 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post('/users', (req, res) => {
-    console.log(req.body);
-    res.send('testing!');
+    const user = new User(req.body);
+
+    user.save().then(() => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(error);
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 })
 
 app.listen(port, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -6,14 +6,37 @@ const User = mongoose.model('User', {
         type: String,
         required: true,
         trim: true
-    },
-    age: {
+    }, age: {
         type: Number,
-        default: 0,
+        default: 20,
         validate(value) {
             if(value < 0) {
                 throw new Error('Age can not less than 0');
             }
         }
+    }, email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        default: 'smrhrbi@gmail.com',
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                console.log('not email.')
+                th
+            }
+        }
+    }, password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('password cant contain "Password"');
+            }
+        }
     }
 });
+
+module.exports = User;
